c17b80cd7dcaed56475a8735b1d1d1a63bc92584
dwikrisnandi <dwikrisnandi8@gmail.com>
2026-08-22 15:08:28 +0700

    chore: add migration script in api dir

---
+++ api/migrate_tugas_override.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise');
+(async () => {
+  const c = await mysql.createConnection({host:'localhost',user:'root',password:'aku',database:'siakad_dkn'});
+  try {
+    await c.execute('ALTER TABLE course_grades ADD COLUMN tugas_override INT DEFAULT NULL');
+    console.log('Column tugas_override added successfully!');
+  } catch(e) {
+    if (e.code === 'ER_DUP_FIELDNAME') {
+      console.log('Column tugas_override already exists, skipping.');
+    } else {
+      throw e;
+    }
+  }
+  await c.end();
+})();
